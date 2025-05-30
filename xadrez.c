2e893f47a50745cf44b0c60e60b79123542551f9
Anderson Duarte <57947382+OverclockerBR@users.noreply.github.com>
2025-05-30 01:05:41 +0000

    Estrutura do bispo

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,50 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
+    //Bispo: 5 casas na diagonal superior direita
+    //Torre: 5 casas para a direita
+    //Rainha: 8 casas para a esquerda
+    char bispo, torre, rainha;
+    int casaBispo, casaTorre, casaRainha;
+    int escolha;
+
+    printf("Qual peça quer mover?\n\n");
+
+    printf("*** 1 - BISPO.\n");
+    printf("*** 2 - TORRE.\n");
+    printf("*** 3 - RAINHA.\n");
+
+    scanf("%d", &escolha);
+
+    switch (escolha){
+    case 1:
+        int escolhaBispo;
+        
+        printf("Para direita ou esquerda?\n\n");
+        printf("*** 1 - Direita.\n");
+        printf("*** 2 - Esquerda.\n");
+
+        scanf("%d", &escolhaBispo);
+
+        do {
+            for (casaBispo = 0; casaBispo <= 5; casaBispo++)
+            {
+                printf("Direita, ");
+                printf("cima\n\n");
+
+                printf("Moveu 5x na diagonal superior direita.\n\n");
+
+            } while (escolhaBispo != 1 && escolhaBispo != '\n');
+        }
+
+            break;
+
+    default:
+        break;
+    
+
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
